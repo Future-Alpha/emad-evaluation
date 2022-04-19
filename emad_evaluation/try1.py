df3c7ab4e0d7ea0a52dba1f532813c979064641f
emad-almajdalawi <majdalawi97@yahoo.com>
2022-04-20 02:48:27 +0300

    feat: (evaluate) add suggestions

--- emad_evaluation/try1.py
+++ emad_evaluation/try1.py
@@ -15,7 +15,7 @@ def draw(virtual_mouse_matrix):
 
 
 def clear():
-    '''cleare the doawn lines'''
+    """cleare the doawn lines"""
     pass
 
 
@@ -28,4 +28,16 @@ def evaluate(line_matrix, dots_matrix):
     """
     grid_length = 32
     grid = np.ones((grid_length, grid_length, 4))
+
+    # https://stackoverflow.com/questions/20402109/calculating-percentage-error-by-comparing-two-arrays
+    a = np.array([1, 2, 3, 4, 5, 6, 7])
+    b = np.array([1, 2, 3, 5, 5, 6, 7])
+    error = np.mean(a != b)
+
+    # or
+    import difflib
+
+    pr = difflib.SequenceMatcher(None, array1, array2)
+    print(pr.ratio())
+
     pass
