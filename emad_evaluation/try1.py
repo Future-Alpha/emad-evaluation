d135ca20a055f3be86a6f4e9a8f7a4e7f4b03d47
emad-almajdalawi <majdalawi97@yahoo.com>
2022-04-20 02:42:21 +0300

    fix: add notes

--- emad_evaluation/try1.py
+++ emad_evaluation/try1.py
@@ -1,10 +1,31 @@
 import pygame
-import numpy
+import numpy as np
 
 
 def draw(virtual_mouse_matrix):
+    """
+    In our game loop:
+    If left mouse button pressed:
+        Fetch the click position in our window
+        If previous_point not equal to None:
+            Draw a line from previous point to click position
+        Set previous_point = click position
+    """
+    pass
+
+
+def clear():
+    '''cleare the doawn lines'''
     pass
 
 
 def evaluate(line_matrix, dots_matrix):
+    """
+    screan --> grid 32x32
+    each point is one square from the grid
+    if the line went over all point squares in the right order --> pass
+        else calculate the orecision
+    """
+    grid_length = 32
+    grid = np.ones((grid_length, grid_length, 4))
     pass
